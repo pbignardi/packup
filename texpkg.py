6cac0f15efc438c2de301ece3ae675975ca4419c
Paolo Bignardi <paolo.bigno@gmail.com>
2022-04-12 16:59:43 +0200

    Addedd config command
    Config function to create a config file and save to disk.
    Save the:
    -texmfhome: path to the local packages
    -pkg_db: database file path for installed packages

--- texpkg.py
+++ texpkg.py
@@ -3,6 +3,36 @@ import os, sys, typer
 TEXMFHOME = "~/Library/texmf/"
 app = typer.Typer()
 
+def config(
+    texmfhome: str, 
+    pkg_db: Optional[str] = typer.Argument(None),
+    mktree: bool = typer.Option(False, help="Create the local packages dir tree"), 
+    force: bool = typer.Option(False, help="Overwrite current setting file")):
+    """
+    Set local packages path.
+
+    Optionally: set packages database file
+    """
+    configs = {
+        "texmfhome": texmfhome
+    }
+    
+    if pkg_db:
+        config["pkg_db"] = pkg_db
+    else:
+        config["pkg_db"] = ".pkg.db"
+
+    if mktree:
+        # TODO: implement mktree
+        pass
+
+    try:
+        if not os.path.isfile(".config.json") or force:
+            with open(".config.json","w") as file:
+                json.dump(configs, file)
+    except:
+        console.print("[error]Error:[/] cannot write settings to JSON file!")
+
 def help():
     print("TeXPKG: deploy and maintain your TeX packages")
 
