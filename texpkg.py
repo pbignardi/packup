80208d1ee8fa79e04080c5d3ccc67b0fa99b0260
Paolo Bignardi <paolo.bigno@gmail.com>
2022-04-12 17:05:54 +0200

    Removed typer decorators

--- texpkg.py
+++ texpkg.py
@@ -53,18 +53,12 @@ def help():
 def remove(pkg: str):
     pass
 
-@app.command()
 def update(pkg: str):
     pass
 
-@app.command()
 def install(input_file: str):
     print(f"Installing at {input_file}")
     pass
-
-def main(command, input=None, output=None):
-    # dispatch command
-    return
 
 if __name__ == "__main__":
     app()
